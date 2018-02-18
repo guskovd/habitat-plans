266999b22906be95c14e89d39ba3ac36ca9e8993
Danil Guskov <d.guskov@corp.mail.ru>
2018-02-18 12:45:35 +0300

    switch to core deps

--- remacs/plan.sh
+++ remacs/plan.sh
@@ -27,17 +27,17 @@ pkg_deps=(
     core/giflib
     core/libtiff
     core/libjpeg-turbo
-    xorg/xlib/1.6.5/20170607234030
-    xorg/libxcb/1.12/20170607233918
-    xorg/libXau/1.0.8/20170607233814
-    xorg/libXdmcp/1.1.2/20170607233905
-    xorg/libXt/1.1.5/20170607234359
-    xorg/libice/1.0.9/20170607233827
-    xorg/libsm/1.2.2/20170607233852
-    xorg/libxext/1.3.3/20170607234437
-    xorg/libxrender/0.9.10/20170607234342
-    xorg/libxmu/1.1.2/20170607234455
-    guskovd/libxpm/3.5.4.2/20180217171952
+    core/xlib
+    core/libxcb
+    core/libxau
+    core/libxdmcp
+    core/libxt
+    core/libice
+    core/libsm
+    core/libxext
+    core/libxrender
+    core/libxmu
+    # guskovd/libxpm/3.5.4.2/20180217171952
 )
 pkg_build_deps=(
     core/rust-nightly
@@ -51,11 +51,10 @@ pkg_build_deps=(
     core/zlib
     core/gmp
     core/pkg-config
-    xorg/xproto/7.0.31/20170607233653
-    xorg/kbproto/1.0.7/20170607233756
-    xorg/libpthread-stubs/0.4/20170607233627
-    xorg/renderproto/0.11.1/20170607233728
-    xorg/renderproto/0.11.1/20170607233728
+    core/xproto
+    core/kbproto
+    core/libpthread-stubs
+    core/renderproto
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
@@ -87,7 +86,7 @@ do_clean() {
 
 do_build() {
     ./autogen.sh
-    ./configure --with-gnutls=no --with-xft --with-modules --with-x-toolkit=gtk2 --with-gconf --without-gsettings --without-makeinfo RUSTFLAGS="-g" --prefix="$pkg_prefix"
+    ./configure --with-gnutls=no --with-xft --with-modules --with-x-toolkit=gtk2 --with-gconf --without-gsettings --without-makeinfo RUSTFLAGS="-g" --prefix="$pkg_prefix" --with-xpm=no
     make
 }
 
