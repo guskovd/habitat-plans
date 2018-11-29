9daf8c028305fcece50b09347df017593ebbb16b
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-29 21:28:50 +0300

    my plans

--- travis/build.sh
+++ travis/build.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 
-# mkdir -p $HOME/.hab/cache/keys
+export HAB_ORIGIN="guskovd"
 
-# export HAB_ORIGIN="guskovd"
-# openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab.tar.enc -out .hab.tar -d
-
-# tar xvf .hab.tar
-# systeminfo
-# cp -rf .hab /c/
+cp -rf .hab /c 
 
 choco install habitat
 hab studio build habitat/selenoid-bin
