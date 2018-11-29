c0362170efc0e88fe062411083abbaf2aaeead8c
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-29 21:05:27 +0300

    my plans

--- travis/build.sh
+++ travis/build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 
-mkdir -p $HOME/.hab/cache/keys
+# mkdir -p $HOME/.hab/cache/keys
 
-export HAB_ORIGIN="guskovd"
+# export HAB_ORIGIN="guskovd"
+# openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab.tar.enc -out .hab.tar -d
 
-openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab.tar.enc -out .hab.tar -d
 # tar xvf .hab.tar
-
 # systeminfo
 # cp -rf .hab /c/
 
