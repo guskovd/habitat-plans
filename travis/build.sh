855abef3cb4c828d3827585b706697f5dc520b37
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-29 20:23:26 +0300

    my plans

--- travis/build.sh
+++ travis/build.sh
@@ -3,9 +3,10 @@
 
 mkdir -p $HOME/.hab/cache/keys
 
+export HAB_ORIGIN="guskovd"
+
 openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab/cache/keys/guskovd-20170522132207.pub.enc -out $HOME/.hab/cache/keys/guskovd-20170522132207.pub -d
+openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab/cache/keys/guskovd-20170522132207.sig.key.enc -out $HOME/.hab/cache/keys/guskovd-20170522132207.sig.key -d
 
-# openssl aes-256-cbc -K $encrypted_0a6446eb3ae3_key -iv $encrypted_0a6446eb3ae3_iv -in .hab/cache/keys/guskovd-20170522132207.pub.enc -out $HOME/.hab/cache/keys/guskovd-20170522132207.pub -d
-# openssl aes-256-cbc -K $encrypted_0a6446eb3ae3_key -iv $encrypted_0a6446eb3ae3_iv -in .hab/cache/keys/guskovd-20170522132207.sig.key.enc -out $HOME/.hab/cache/keys/guskovd-20170522132207.sig.key.enc -d
-# choco install habitat
-
+choco install habitat
+hab studio build habitat/selenoid-bin
