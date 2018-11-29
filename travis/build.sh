4b5cc81cfd5e3ded79484de2e7c1d07b18c7eb64
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-29 20:34:29 +0300

    my plans

--- travis/build.sh
+++ travis/build.sh
@@ -6,7 +6,7 @@ mkdir -p $HOME/.hab/cache/keys
 export HAB_ORIGIN="guskovd"
 
 openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab.tar.enc -out .hab.tar -d
-tar xvf secrets.tar
+tar xvf .hab.tar
 
 cp .hab $HOME -rf
 
