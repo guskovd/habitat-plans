870f8d28b6ace97f3f191d715d3d27eca895c0ec
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-29 20:48:26 +0300

    my plans

--- travis/build.sh
+++ travis/build.sh
@@ -8,9 +8,7 @@ export HAB_ORIGIN="guskovd"
 openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab.tar.enc -out .hab.tar -d
 tar xvf .hab.tar
 
-ls $HOME -lha
+cp .hab C:/ -rf
 
-# cp .hab $HOME -rf
-
-# choco install habitat
-# hab studio build habitat/selenoid-bin
+choco install habitat
+hab studio build habitat/selenoid-bin
