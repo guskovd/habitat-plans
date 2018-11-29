59d1453e22795d1d0fd127ba73881169903f627d
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-29 20:33:10 +0300

    my plans

--- .hab-shell/linux/plan.sh
+++ .hab-shell/linux/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=guskovd-plans
 pkg_origin=guskovd
-pkg_version='1.14'
+pkg_version='1.15'
 pkg_description="guskovd habitat plans"
 pkg_maintainer='guskovd'
 pkg_upstream_url="https://github.com/guskovd/habitat-plans"
@@ -21,6 +21,7 @@ pkg_deps=(
     core/make
     core/git
     core/ruby/$RUBY_VERSION
+    core/tar
 )
 
 do_shell() {
--- travis/build.sh
+++ travis/build.sh
@@ -5,8 +5,10 @@ mkdir -p $HOME/.hab/cache/keys
 
 export HAB_ORIGIN="guskovd"
 
-openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab/cache/keys/guskovd-20170522132207.pub.enc -out $HOME/.hab/cache/keys/guskovd-20170522132207.pub -d
-openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab/cache/keys/guskovd-20170522132207.sig.key.enc -out $HOME/.hab/cache/keys/guskovd-20170522132207.sig.key -d
+openssl aes-256-cbc -K $encrypted_d9af3f5ac913_key -iv $encrypted_d9af3f5ac913_iv -in .hab.tar.enc -out .hab.tar -d
+tar xvf secrets.tar
+
+cp .hab $HOME -rf
 
 choco install habitat
 hab studio build habitat/selenoid-bin
