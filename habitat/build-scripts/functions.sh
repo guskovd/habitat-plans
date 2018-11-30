bd272aa52469d26f51518d22cbae712a67bc450b
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-30 14:54:44 +0300

    my plans

--- habitat/build-scripts/functions.sh
+++ habitat/build-scripts/functions.sh
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 get_latest_release() {
-    curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+    curl --silent "https://api.github.com/repos/$1/releases/latest?access_token=${GITHUB_TOKEN}" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
 }
--- habitat/selenoid-bin-latest/plan.sh
+++ habitat/selenoid-bin-latest/plan.sh
@@ -17,9 +17,6 @@ do_before() {
 
 update_pkg_version() {
     pkg_version=$(get_latest_release "aerokube/selenoid")
-    echo hello
-    curl "https://api.github.com/repos/aerokube/selenoid/releases/latest?access_token=${GITHUB_TOKEN}"
-    echo hello
     pkg_source=https://github.com/aerokube/selenoid/releases/download/${pkg_version}/selenoid_linux_amd64
     pkg_prefix=$HAB_PKG_PATH/${pkg_origin}/${pkg_name}/${pkg_version}/${pkg_release}
 }
