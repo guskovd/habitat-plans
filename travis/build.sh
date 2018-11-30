da80bc096e49352f8350b596aa3cdcd214308472
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-30 13:27:02 +0300

    my plans

--- travis/build.sh
+++ travis/build.sh
@@ -6,14 +6,18 @@ export HAB_ORIGIN="guskovd"
 if [[ "$(uname -s)" == "Linux" ]]; then
     cp -rf .hab $HOME/.hab
     curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash
+    results=results
+    last_build=results/last_build.sh
 else
     cp -rf .hab /c/hab
     choco install habitat
+    results=habitat/${HAB_PKG}/results
+    last_build=habitat/${HAB_PKG}/results/last_build.ps1
 fi
 
 hab studio build habitat/${HAB_PKG}
 
-pkg_artifact=$(cat habitat/${HAB_PKG}/results/last_build* | grep pkg_artifact | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
-pkg_ident=$(cat habitat/${HAB_PKG}/results/last_build.* | grep pkg_ident | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
+pkg_artifact=$(cat $last_build | grep pkg_artifact | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
+pkg_ident=$(cat $last_build | grep pkg_ident | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
 
-hab pkg upload habitat/${HAB_PKG}/results/$pkg_artifact
+hab pkg upload $results/$pkg_artifact
