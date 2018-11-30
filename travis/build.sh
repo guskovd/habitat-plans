43edb9e7e2cc758aed29c41900f4ce1dc16faea6
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-30 12:57:49 +0300

    my plans

--- travis/build.sh
+++ travis/build.sh
@@ -6,9 +6,9 @@ export HAB_ORIGIN="guskovd"
 cp -rf .hab /c/hab
 
 choco install habitat
-hab studio build habitat/selenoid-bin
+hab studio build habitat/${HAB_PKG}
 
-pkg_artifact=$(cat habitat/selenoid-bin/results/last_build.ps1 | grep pkg_artifact | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
-pkg_ident=$(cat habitat/selenoid-bin/results/last_build.ps1 | grep pkg_ident | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
+pkg_artifact=$(cat habitat/${HAB_PKG}/results/last_build* | grep pkg_artifact | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
+pkg_ident=$(cat habitat/${HAB_PKG}/results/last_build.* | grep pkg_ident | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
 
-hab pkg upload habitat/selenoid-bin/results/$pkg_artifact
+hab pkg upload habitat/${HAB_PKG}/results/$pkg_artifact
