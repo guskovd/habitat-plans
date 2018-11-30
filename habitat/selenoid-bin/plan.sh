db4b517b7a1c2ee82b674606901c3c57e8404fc6
Danil Guskov <d.guskov@corp.mail.ru>
2018-11-30 13:36:04 +0300

    my plans

--- habitat/selenoid-bin/plan.sh
+++ habitat/selenoid-bin/plan.sh
@@ -9,12 +9,6 @@ pkg_source=https://github.com/aerokube/selenoid/releases/download/${pkg_version}
 pkg_shasum=8e7f36b6212c1ad4cb5e83fed4c2b12ea029555cf2e015cbba3eec2e4daf8491
 pkg_bin_dirs=(bin)
 
-pkg_svc_user="root"
-
-pkg_exports=(
-    [selenoid]=selenoid
-)
-
 do_unpack () {
     return 0
 }
