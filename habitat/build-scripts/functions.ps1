73446aaf34df137b115a5298b330f0a43d3e6dc2
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-06 17:01:06 +0300

    selenoid-bin-latest

--- habitat/build-scripts/functions.ps1
+++ habitat/build-scripts/functions.ps1
@@ -1,3 +1,5 @@
 function getLatestRelease ($repo) {
-    return $(Invoke-RestMethod -Uri "https://api.github.com/repos/$repo/releases/latest?access_token=$GITHUB_TOKEN").tag_name
+    $uri = "https://api.github.com/repos/$repo/releases/latest?access_token=$GITHUB_TOKEN"
+    echo "web request uri: $uri"
+    return $(Invoke-RestMethod -Uri "$uri").tag_name
 }
