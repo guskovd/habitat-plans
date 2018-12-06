6c6023b86b4b60d7f60d0599b4672f8811457fb1
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-06 20:11:39 +0300

    change copyrights

--- habitat/geckodriver-bin/plan.sh
+++ habitat/geckodriver-bin/plan.sh
@@ -4,20 +4,16 @@ pkg_version='0.23.0'
 pkg_description="Geckodriver binary"
 pkg_maintainer='Danil Guskov'
 pkg_license=('LGPL-2.1')
-pkg_upstream_url="https://github.com/aerokube/selenoid"
-pkg_source=https://github.com/aerokube/selenoid/releases/download/${pkg_version}/selenoid_linux_amd64
-pkg_shasum=8e7f36b6212c1ad4cb5e83fed4c2b12ea029555cf2e015cbba3eec2e4daf8491
+pkg_upstream_url="https://github.com/mozilla/geckodriver"
+pkg_source="https://github.com/mozilla/geckodriver/releases/download/v$pkg_version/geckodriver-v$pkg_version-linux64.tar.gz"
+pkg_shasum=2abf02cb69b48f2ba65ea344b752ff547e5431659aad80b03bf68cdb4f8df14b
 pkg_bin_dirs=(bin)
-
-do_unpack () {
-    return 0
-}
 
 do_build() {
     return 0
 }
 
 do_install () {
-    cp $HAB_CACHE_SRC_PATH/selenoid_linux_amd64 $pkg_prefix/bin/selenoid
-    chmod '+x' $pkg_prefix/bin/selenoid
+    cp $HAB_CACHE_SRC_PATH/geckodriver $pkg_prefix/bin/geckodriver
+    chmod '+x' $pkg_prefix/bin/geckodriver
 }
