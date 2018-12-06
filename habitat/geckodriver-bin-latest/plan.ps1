e172e585c9145b30c8c8868255369d0c072d004e
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-07 00:30:16 +0300

    fix plans

--- habitat/geckodriver-bin-latest/plan.ps1
+++ habitat/geckodriver-bin-latest/plan.ps1
@@ -6,7 +6,7 @@ $pkg_version="_set_from_git_"
 function Invoke-Before {
     $tag=getLatestRelease("mozilla/geckodriver")
     $pkg_version=$tag.Substring(1, $tag.Length-1) # strip first v symbol, e.g v0.23.0
-    $script:pkg_version=getLatestRelease("mozilla/geckodriver")
+    $script:pkg_version=$pkg_version
     $script:pkg_source="https://github.com/mozilla/geckodriver/releases/download/v$pkg_version/geckodriver-v$pkg_version-win64.zip"
     $script:pkg_prefix="$HAB_PKG_PATH\${pkg_origin}\${pkg_name}\${pkg_version}\${pkg_release}"
     $script:pkg_dirname="$pkg_name-$pkg_version"
