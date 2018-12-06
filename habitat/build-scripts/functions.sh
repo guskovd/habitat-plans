4b31d41665560aef736ab94b32294c2554f12ee0
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-07 00:25:29 +0300

    fix plans

--- habitat/build-scripts/functions.sh
+++ habitat/build-scripts/functions.sh
@@ -4,7 +4,5 @@
 . ../tmp/env.sh
 
 get_latest_release() {
-    uri="https://api.github.com/repos/$1/releases/latest?access_token=${GITHUB_TOKEN}"
-    echo "web request uri: $uri"
-    curl --silent $uri | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
+    curl --silent "https://api.github.com/repos/$1/releases/latest?access_token=${GITHUB_TOKEN}" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'
 }
--- habitat/geckodriver-bin-latest/plan.ps1
+++ habitat/geckodriver-bin-latest/plan.ps1
@@ -7,7 +7,7 @@ function Invoke-Before {
     $tag=getLatestRelease("mozilla/geckodriver")
     $pkg_version=$tag.Substring(1, $tag.Length-1) # strip first v symbol, e.g v0.23.0
     $script:pkg_version=getLatestRelease("mozilla/geckodriver")
-    $script:pkg_source="https://github.com/mozilla/geckodriver/releases/download/$pkg_version/geckodriver-v$pkg_version-win64.zip"
+    $script:pkg_source="https://github.com/mozilla/geckodriver/releases/download/v$pkg_version/geckodriver-v$pkg_version-win64.zip"
     $script:pkg_prefix="$HAB_PKG_PATH\${pkg_origin}\${pkg_name}\${pkg_version}\${pkg_release}"
     $script:pkg_dirname="$pkg_name-$pkg_version"
     $script:pkg_artifact="$HAB_CACHE_ARTIFACT_PATH/$pkg_origin-$pkg_name-$pkg_version-$pkg_release-$pkg_target.$_artifact_ext"
