e34e8880f89290f72ce78a6f87042a069bffc912
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-06 18:42:18 +0300

    selenoid-bin-latest

--- habitat/build-scripts/functions.ps1
+++ habitat/build-scripts/functions.ps1
@@ -1,3 +1,5 @@
+. ../tmp/env.ps1
+
 function getLatestRelease ($repo) {
     $uri = "https://api.github.com/repos/$repo/releases/latest?access_token=$GITHUB_TOKEN"
     echo "web request uri: $uri"
--- habitat/build-scripts/functions.sh
+++ habitat/build-scripts/functions.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 
+. ../tmp/env.sh
+
 get_latest_release() {
     uri="https://api.github.com/repos/$1/releases/latest?access_token=${GITHUB_TOKEN}"
     echo "web request uri: $uri"
