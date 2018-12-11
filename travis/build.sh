e45d721466a6d6baebb7b3210838cb274a2cb8af
Danil Guskov <d.guskov@corp.mail.ru>
2018-12-11 16:07:54 +0300

    fix travis build via windows & linux channels

--- travis/build.sh
+++ travis/build.sh
@@ -7,12 +7,14 @@ export HAB_ORIGIN="guskovd"
 
 if [[ "$(uname -s)" == "Linux" ]]; then # Linux setup
     echo "export GITHUB_TOKEN=$GITHUB_TOKEN" > habitat/tmp/env.sh
+    os_name="linux"
     cp -rf .hab $HOME/.hab
     curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash
     results=results
     last_build=results/last_build.env
 else # Windows setup
     echo "\$env:GITHUB_TOKEN=\"$GITHUB_TOKEN\"" > habitat/tmp/env.ps1
+    os_name="windows"
     cp -rf .hab /c/hab
     choco install habitat
     results=habitat/${HAB_PKG}/results
@@ -27,9 +29,11 @@ pkg_origin=$(cat $last_build | grep pkg_origin | awk -F '=' '{print $2}' | sed $
 pkg_version=$(cat $last_build | grep pkg_version | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
 pkg_name=$(cat $last_build | grep pkg_name | awk -F '=' '{print $2}' | sed $'s/[\r:\"]//g')
 
-if [[ ! $(hab pkg search $pkg_origin/$pkg_name | grep $pkg_origin/$pkg_name/$pkg_version) ]]; then
+search_result=$(hab pkg search $pkg_origin/$pkg_name | grep $pkg_origin/$pkg_name/$pkg_version)
+if [[ ! $search_result ]] || [[ ! $(hab pkg channels $search_result | grep $os_name) ]]; then
     echo "Uploading artifact ..."
     hab pkg upload $results/$pkg_artifact
+    hab pkg promote $pkg_ident $os_name
     hab pkg promote $pkg_ident stable
 else
     echo "Package exist on hab depot. Skip"
