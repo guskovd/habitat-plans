c7e8ab20d2a3538e9a52f30ea1123d3ef2fcd3c4
Danil Guskov <d.guskov@corp.mail.ru>
2019-01-16 22:15:29 +0300

    salt plan

---
+++ habitat/salt/plan.sh
@@ -0,0 +1,43 @@
+pkg_name=salt
+pkg_version=v2018.3.3
+pkg_origin=guskovd
+pkg_license=('Apache-2.0')
+pkg_maintainer="Danil Guskov <guskovd86@mail.ru>"
+pkg_license=("Apache-2.0")
+pkg_upstream_url=https://github.com/saltstack/salt
+pkg_deps=(
+    core/python2
+    core/virtualenv
+    core/zeromq
+    core/openssl-musl
+)
+pkg_build_deps=(
+    core/gcc
+)
+
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(bin)
+
+do_prepare() {
+    virtualenv "$pkg_prefix"
+    source "$pkg_prefix/bin/activate"
+}
+
+do_build() {
+    return 0
+}
+
+do_install() {
+    pip install "$pkg_name==$pkg_version"
+    pip freeze > "$pkg_prefix/requirements.txt"
+    ln -sf $(pkg_path_for core/openssl-musl)/lib/libcrypto.so.1.0.0 ${pkg_prefix}/lib/libcrypto.so.10
+}
+
+do_strip() {
+    return 0
+}
+
+do_setup_environment() {
+    push_runtime_env LD_LIBRARY_PATH "$(pkg_path_for core/openssl)/lib"
+    push_runtime_env LD_LIBRARY_PATH "${pkg_prefix}/lib"
+}
