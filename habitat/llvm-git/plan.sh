706de7f964e204ab1221094cf2f336fc3958515c
Danil Guskov <d.guskov@corp.mail.ru>
2019-02-10 02:02:25 +0300

    llvm

---
+++ habitat/llvm-git/plan.sh
@@ -0,0 +1,8 @@
+source ../llvm/plan.sh
+
+pkg_name=llvm-git
+pkg_origin=guskovd
+pkg_version=8c2a52db8a1ab1287833bc6b6c7a229a4a6c0ad0
+pkg_filename="${pkg_name}-${pkg_version}.src.tar.gz"
+pkg_source="https://github.com/llvm-mirror/llvm/archive/$pkg_version.tar.gz"
+pkg_shasum="c49d484aa9fec1f5d3c8b8713c1cd1945de0b3f23b35581032bb437342b75f08"
