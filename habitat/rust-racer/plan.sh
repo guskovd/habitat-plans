e3595e413e326abd420fdcaa48766081b0191a08
Danil Guskov <d.guskov@corp.mail.ru>
2019-03-01 20:44:31 +0300

    new rust racer

--- habitat/rust-racer/plan.sh
+++ habitat/rust-racer/plan.sh
@@ -1,23 +1,30 @@
 pkg_name=rust-racer
 pkg_origin=guskovd
-pkg_version=2.1.16
+pkg_version=2.1.19
 pkg_license=('BSD')
 pkg_bin_dirs=(bin)
 pkg_maintainer="Danil Guskov"
 pkg_upstream_url="https://github.com/racer-rust/racer"
 pkg_description="Rust Code Completion utility "
 
-pkg_deps=(core/glibc core/gcc-libs core/openssl)
 pkg_build_deps=(
     guskovd/rust-nightly
     core/gcc
     core/pkg-config
-    core/openssl
     core/make
+    core/musl
+    # core/glibc
+    # core/gcc-libs
 )
 
+do_prepare() {
+    export LD_LIBRARY_PATH=$(pkg_path_for gcc)/lib
+    export rustc_target="x86_64-unknown-linux-musl"
+    build_line "Setting rustc_target=$rustc_target"
+}
+
 do_build() {
-    cargo -Z unstable-options install racer --root "${pkg_prefix}" --vers "${pkg_version}"
+    cargo -Z unstable-options install --target=$rustc_target racer --root "${pkg_prefix}" --vers "${pkg_version}"
 }
 
 do_install() {
@@ -28,6 +35,6 @@ do_download() {
     return 0
 }
 
-do_setup_environment() {
-    push_runtime_env LD_LIBRARY_PATH "$(pkg_path_for core/gcc-libs)/lib"
-}
+# do_setup_environment() {
+#     push_runtime_env LD_LIBRARY_PATH "$(pkg_path_for core/gcc-libs)/lib"
+# }
