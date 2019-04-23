f93ec38eb49dd5173ec6dc25bc7cb8a72d33cfd2
Danil Guskov <d.guskov@corp.mail.ru>
2019-04-23 19:55:31 +0300

    new hab plans

--- habitat/jenkins-jobs-dsl-rest/plan.sh
+++ habitat/jenkins-jobs-dsl-rest/plan.sh
@@ -8,11 +8,10 @@ pkg_source=https://github.com/sheehan/job-dsl-rest-example/archive/master.tar.gz
 pkg_upstream_url=https://github.com/sheehan/job-dsl-rest-example
 pkg_shasum=c33a2a08cbe6e2036542d51f6df02973bc23e86422ff6ccfea4d8eba11f7d354
 pkg_dirname=job-dsl-rest-example-${pkg_version}
+
 pkg_deps=(
-    core/jre8
-    core/gradle
-    core/hab
-    core/bash
+    core/jre8/8.172.0/20180608184654
+    core/gradle/4.3.1/20180610000843
 )
 pkg_bin_dirs=(bin)
 pkg_lib_dirs=(lib)
@@ -35,19 +34,19 @@ _install_dependency() {
     local dep="${1}"
     if [[ -z "${NO_INSTALL_DEPS:-}" ]]; then
 
-    # Enable --ignore-local if invoked with HAB_FEAT_IGNORE_LOCAL in
-    # the environment, set to either "true" or "TRUE" (features are
-    # not currently enabled by the mere presence of an environment variable)
-    if [[ "${HAB_FEAT_IGNORE_LOCAL:-}" = "true" ||
-              "${HAB_FEAT_IGNORE_LOCAL:-}" = "TRUE" ]]; then
-        IGNORE_LOCAL="--ignore-local"
+	# Enable --ignore-local if invoked with HAB_FEAT_IGNORE_LOCAL in
+	# the environment, set to either "true" or "TRUE" (features are
+	# not currently enabled by the mere presence of an environment variable)
+	if [[ "${HAB_FEAT_IGNORE_LOCAL:-}" = "true" ||
+		  "${HAB_FEAT_IGNORE_LOCAL:-}" = "TRUE" ]]; then
+            IGNORE_LOCAL="--ignore-local"
+	fi
+	$HAB_BIN pkg path "$dep" || $HAB_BIN install -u $HAB_BLDR_URL --channel $HAB_BLDR_CHANNEL ${IGNORE_LOCAL:-} "$dep" || {
+		if [[ "$HAB_BLDR_CHANNEL" != "$FALLBACK_CHANNEL" ]]; then
+		    build_line "Trying to install '$dep' from '$FALLBACK_CHANNEL'"
+		    $HAB_BIN install -u $HAB_BLDR_URL --channel "$FALLBACK_CHANNEL" ${IGNORE_LOCAL:-} "$dep" || true
+		fi
+	    }
     fi
-    $HAB_BIN pkg path "$dep" || $HAB_BIN install -u $HAB_BLDR_URL --channel $HAB_BLDR_CHANNEL ${IGNORE_LOCAL:-} "$dep" || {
-      if [[ "$HAB_BLDR_CHANNEL" != "$FALLBACK_CHANNEL" ]]; then
-        build_line "Trying to install '$dep' from '$FALLBACK_CHANNEL'"
-        $HAB_BIN install -u $HAB_BLDR_URL --channel "$FALLBACK_CHANNEL" ${IGNORE_LOCAL:-} "$dep" || true
-      fi
-    }
-  fi
-  return 0
+    return 0
 }
