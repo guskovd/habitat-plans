0ab7f0386d254ddbe4b024e9cfddfc3c6ea31121
Danil Guskov <d.guskov@corp.mail.ru>
2019-06-28 16:49:04 +0300

    new plans

---
+++ habitat/firefox-developer-edtition/plan.sh
@@ -0,0 +1,6 @@
+. ../firefox/plan.sh
+
+pkg_name=firefox-developer-edtition
+pkg_version='68.0b9'
+pkg_source=http://archive.mozilla.org/pub/devedition/releases/${pkg_version}/linux-x86_64/en-US/firefox-${pkg_version}.tar.bz2
+pkg_shasum=3a38e788b3fb0722ec7334ba8d605a70f74acb02fc0aa39cea3c7c05978d2242
---
+++ habitat/firefox/plan.sh
@@ -0,0 +1,18 @@
+pkg_name=firefox-bin
+pkg_origin=guskovd
+pkg_version='67.0.4'
+pkg_description="Firefox Binary"
+pkg_maintainer='Danil Guskov'
+pkg_license=('LGPL-2.1')
+pkg_upstream_url="https://www.mozilla.org/en-US/firefox/"
+pkg_source=https://ftp.mozilla.org/pub/firefox/releases/${pkg_version}/linux-x86_64/en-US/firefox-${pkg_version}.tar.bz2
+pkg_shasum=3bde74d9d958552081fad3b9c34db636abecedd48d947910d5f74e367af523b7
+pkg_bin_dirs=(bin)
+
+do_build() {
+    return 0
+}
+
+do_install () {
+    cp -rf $HAB_CACHE_SRC_PATH/firefox/* $pkg_prefix/bin
+}
