fd8b710f9ccf9e78de1ac2322b897e7bb1f95b15
Danil Guskov <d.guskov@corp.mail.ru>
2019-07-18 10:25:16 +0300

    firefox update

--- habitat/firefox-devedition/plan.sh
+++ habitat/firefox-devedition/plan.sh
@@ -1,6 +1,6 @@
 . ../firefox/plan.sh
 
 pkg_name=firefox-devedition
-pkg_version='68.0b9'
+pkg_version='68.0b14'
 pkg_source=http://archive.mozilla.org/pub/devedition/releases/${pkg_version}/linux-x86_64/en-US/firefox-${pkg_version}.tar.bz2
-pkg_shasum=3a38e788b3fb0722ec7334ba8d605a70f74acb02fc0aa39cea3c7c05978d2242
+pkg_shasum=6f9c04b1032df54280fb5819613c0e13c1ad5abc7dccd182589a0210608ffebf
